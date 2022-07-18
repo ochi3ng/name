b0d759406b5e6218e2669d5bdb2d9168590df193
ochi3ng <johnochi3ng@gmail.com>
2022-07-18 18:09:40 +0300

    my project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useState,useEffect } from 'react';
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [count, setCount] = useState();
+  const[stop,setStop]=useState(1)
+const Pauze=()=>{
+  setStop(0)
 }
+  useEffect(() => {
+    if (count > 0) {
+      setTimeout(() => setCount(count + stop), 1000);
+    } else {
+      setCount('Times up');
+    }
+  }, [count]);
+  return (
+    <>
+      <div>{count}</div>
+      <button onClick={() => setCount(10)}> start </button>
+      <button onClick={Pauze}> pauze </button>
+    </>)
+}
+  // const [Value,setValue]=useState()
+  //   const[Text,setText]=useState()
+  // const handleClick=()=>{
+  //   if(Text==="Random Number"){
+  //     setText("Hello World");
+  //   }
+  //   else if(Text==="Hello World"){
+  //     setText("Say Your Name");
+  //   }else if(Text==="Say Your Name"){
+  //     setText("Am John");
+  //   }else if(Text==="Am John"){
+  //     setText("Welcome");
+  //   }else if (Text==="Welcome"){
+  //     setText("Thank You");
+  //   }else{
+  //     setText("Random Number")
+  //   }
+    // if(Value===(0)){
+    //   setValue("hello")
+    // }
+ 
+//   return (
+// <React.Fragment>
+//   {/* <h1>{Value}</h1>
+//   <button type='button' className='btn' onClick={handleClick}>Enter</button> */}
+//   <SenderInput/>
+// </React.Fragment>
+//   );
+// }
 
-export default App;
+export default App
---
+++ src/userInput.js
@@ -0,0 +1,20 @@
+// import { useState } from "react"
+
+// function UserInput(){ 
+//     const[Value,setValue]=useState()
+//     const[Display,setDisplay]=useState(false)
+//     const handleClick=()=>{
+//         setDisplay(!Display)
+//     }
+//     const onChange=(event)=>{
+// setValue(event.target.value)
+//     }
+// return(
+// <div>
+//     <div>{Display && Value}</div>
+//     <input onChange={onChange}/>
+//     <button type='button' className='btn' onClick={handleClick}>submit</button>
+// </div>
+// )
+// }
+// export default UserInput
