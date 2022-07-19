e9a55924f13f00262ca75a0c9b1cec7166d9c939
ochi3ng <johnochi3ng@gmail.com>
2022-07-19 12:29:39 +0300

    my changes

--- src/App.js
+++ src/App.js
@@ -1,30 +1,13 @@
 
 import React, { useState} from 'react';
+import SenderInput from './SenderInput';
 function App(){
-  const [Value,setValue]=useState()
-    const[Text,setText]=useState()
-  const handleClick=()=>{
+ 
 
-    if(Text==="Random Number"){
-      setText("Hello World");
-
-    } else if(Text==="Hello World"){
-      setText("Say Your Name");
-    }else if(Text==="Say Your Name"){
-      setText("Am John");
-    }else if(Text==="Am John"){
-      setText("Welcome");
-    }else if (Text==="Welcome"){
-      setText("Thank You");
-    }else{
-      setText("Random Number")
-    }
-  }
   return (
 <React.Fragment>
-  <h1>{Text}</h1>
-  <button type='button' className='btn' onClick={handleClick}>Enter</button>
-  {/* <SenderInput/> */}
+ 
+  <SenderInput/>
 </React.Fragment>
   );
 }
