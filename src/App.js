52eb4cca8591ca6fab19e7743c8f62cabc9daab8
ochi3ng <johnochi3ng@gmail.com>
2022-07-20 10:52:22 +0300

    my tutorial practice

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
 
 import React, { useState} from 'react';
-import SenderInput from './SenderInput';
-function App(){
+export default function App(){
+  const[resourceType,setResourceType]=useState("post")
+  return(
+    <>
+       <hi>{resourceType}</hi>
+    <div>
+      <button onClick={()=>setResourceType("post")}>post</button>
+      <button onClick={()=>setResourceType("edit")}>edit</button>
+      <button onClick={()=>setResourceType("comment")}>comment</button>
+      <button onClick={()=>setResourceType("share")}>share</button>
+      <button onClick={()=>setResourceType("Mark")}>Mark</button>
+      <button onClick={()=>setResourceType("unMark")}>unMark</button>
+      <button onClick={()=>setResourceType("Display")}>Display</button>
+      <button onClick={()=>setResourceType("Zoom")}>Zoom</button>
+      <button onClick={()=>setResourceType("Like")}>Like</button>
+      <button onClick={()=>setResourceType("unLike")}>unLike</button>
+    </div>
  
-
-  return (
-<React.Fragment>
- 
-  <SenderInput/>
-</React.Fragment>
-  );
+    </>
+  )
 }
-
-export default App
